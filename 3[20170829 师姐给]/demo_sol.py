3079a37112903f8d2156961f26414f3286422c4a
Bill Wang <billhhh@163.com>
2017-08-29 13:38:53 +0800

    3[20170829 师姐给]

---
+++ 3[20170829 师姐给]/demo_sol.py
@@ -0,0 +1,22 @@
+import numpy
+import scipy
+from sklearn import datasets
+from sol_classifiers import ogd
+
+iris=datasets.load_iris()
+X_train=iris.data
+Y_train=iris.target
+
+
+X_test=iris.data[45:46]
+Y_test=iris.target[45:46]
+
+
+clf=ogd(eta=0.1)
+clf.fit(X_train,Y_train)
+
+
+print(clf.predict(X_test))
+
+
+
---
+++ 3[20170829 师姐给]/sol_classifiers.py
@@ -0,0 +1,16 @@
+import pysol
+import target_process
+
+class ogd:
+
+	def __init__(self,**params):
+		self.params=params
+
+	
+	def fit(self,X,Y):
+		[Y_new,self.old_to_new,self.new_to_old,num_class]=target_process.translate(Y)
+		self.classifier=pysol.SOL('ogd',num_class,**self.params)
+		self.classifier.fit(X,Y_new)
+	
+	def predict(self, X):
+		return self.classifier.predict(X)
